a4bfa4a5b561df005dd1bdc788189a6c8c383b05
alanak1 <112982685+alanak1@users.noreply.github.com>
2024-04-10 11:12:44 -0300

    Crriação de classes e gerador de códigos aleatórios

--- models/Produto.java
+++ models/Produto.java
@@ -1,3 +1,39 @@
 public class Produto {
-    
+    private float preco;
+    private String descricao;
+    private float avaliacao;
+    private String nomeDoProduto;
+    private int idProduto;
+
+    public float getPreco() {
+        return preco;
+    }
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    public float getAvaliacao() {
+        return avaliacao;
+    }
+    public void setAvaliacao(float avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+    public String getNomeDoProduto() {
+        return nomeDoProduto;
+    }
+    public void setNomeDoProduto(String nomeDoProduto) {
+        this.nomeDoProduto = nomeDoProduto;
+    }
+    public int getIdProduto() {
+        return idProduto;
+    }
+    public void setIdProduto(int idProduto) {
+        this.idProduto = idProduto;
+    }
 }
--- services/GerarCodUnico.java
+++ services/GerarCodUnico.java
@@ -1,3 +1,11 @@
+import java.util.Random;
+
 public class GerarCodUnico {
-       
+    private Random gerador = new Random();
+
+    public void gerarRandom() {
+        for ( int i = 0; i < 10; i++) {
+            System.out.println(gerador.nextInt());
+        }
+    }
 }
